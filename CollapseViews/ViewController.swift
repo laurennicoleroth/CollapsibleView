fee06e3cfd6d81d8a83609d270f0230ddbfdc493
Lauren N. Roth <laurenroth@gmail.com>
2016-11-04 16:40:55 -0400

    Move collapsible into a container view

--- CollapseViews/ViewController.swift
+++ CollapseViews/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var firstHeightConstraint: NSLayoutConstraint!
+
+  var defaultViewHeight: CGFloat?
+  var defaultViewSpacing: CGFloat?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    defaultViewHeight = self.firstHeightConstraint.constant
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func hideSecondLabel(sender: AnyObject) {
+    self.firstHeightConstraint.constant = self.firstHeightConstraint.constant == 0 ? defaultViewHeight! : 0
 
-
+    self.view.setNeedsLayout()
+    
+    // Bonus: animating change
+    UIView.animate(withDuration: 0.3) { () -> Void in
+      self.view.layoutIfNeeded()
+    }
+  }
 }
-
