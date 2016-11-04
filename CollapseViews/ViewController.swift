bcb39049bba23252e8e97e3a7f001e7c7e0dca15
Lauren N. Roth <laurenroth@gmail.com>
2016-11-04 16:22:38 -0400

    Initial Commit

---
+++ CollapseViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollapseViews
+//
+//  Created by Lauren Nicole Roth on 11/4/16.
+//  Copyright © 2016 Skedaddle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
